c151590c34da4c1347d155a5bc1345bdf56c997a
memorybot <mavenread@gmail.com>
2024-06-13 16:43:33 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,23 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '27950e754aa74342b4090b83b1a3e561',
+  rootNotionPageId: 'a1da72c152e54d00ba3104a3a0be36c2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'rdfn penplotter studio',
-  domain: 'rdfn.ch',
-  author: 'Philipp Lehmann',
+  name: '',
+  domain: '',
+  author: '',
 
   // open graph metadata (optional)
-  description: 'A portfolio site for generative art',
+  description: '',
 
   // social usernames (optional)
-  // linkedin: 'ph.lehmann',
-  // instagram: 'ph.lehmann',
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -30,7 +31,7 @@ export default siteConfig({
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+  isPreviewImageSupportEnabled: false,
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
@@ -42,9 +43,7 @@ export default siteConfig({
   // example:
   //
   // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
+  //  '/misc': 'a1da72c152e54d00ba3104a3a0be36c2'},
   pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
@@ -52,13 +51,9 @@ export default siteConfig({
   // navigationStyle: 'default'
   navigationStyle: 'custom',
   navigationLinks: [
-    {
-      title: 'Photoplotter',
-      pageId: 'b870d9084d954356b00c04771ec03677'
-    }, 
-    {
-      title: 'About',
-      pageId: 'c0054a1d000446beb6baed4417066ed9'
-    }
-  ]
+     {
+       title: 'Misc',
+       pageId: 'a1da72c152e54d00ba3104a3a0be36c2'
+     }
+   ]
 })
